7ac14648af7df99a7c6a9251e9e48d5d8618c5d8
Hyungsuk Kang <hyungsukkang@Hyungsuks-MacBook-Pro.local>
2022-08-14 00:30:27 +0900

    update nxink

---
+++ packages/create-nx-workspace/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'create-nx-workspace',
+  testEnvironment: 'node',
+  preset: '../../jest.preset.js',
+};
--- packages/nxink/src/generators/init/schema.d.ts
+++ packages/nxink/src/generators/init/schema.d.ts
@@ -1,7 +1,7 @@
 export default interface Options {
 	/**
 	 * The rustup toolchain channel for projects in this workspace
-	 * @default "stable"
+	 * @default "nightly"
 	 */
 	toolchain?: string;
 }
